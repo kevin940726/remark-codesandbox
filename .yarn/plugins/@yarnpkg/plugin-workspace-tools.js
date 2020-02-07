5da10e76d34b10ce39db43dd02608b5a7e2e8c44
Kai Hao <kevin830726@gmail.com>
2020-02-07 12:20:45 +0800

    Upgrade yarn and plugins

--- .yarn/plugins/@yarnpkg/plugin-workspace-tools.js
+++ .yarn/plugins/@yarnpkg/plugin-workspace-tools.js
@@ -108,10 +108,8 @@ module.exports = {
 
   const foreach_1 = __importDefault(__webpack_require__(1));
 
-  const workspace_1 = __importDefault(__webpack_require__(9));
-
   const plugin = {
-    commands: [foreach_1.default, workspace_1.default]
+    commands: [foreach_1.default]
   }; // eslint-disable-next-line arca/no-default-export
 
   exports.default = plugin;
@@ -210,7 +208,7 @@ module.exports = {
         project,
         workspace: cwdWorkspace
       } = await core_1.Project.find(configuration, this.context.cwd);
-      if (!this.all && !cwdWorkspace) throw new cli_1.WorkspaceRequiredError(this.context.cwd);
+      if (!this.all && !cwdWorkspace) throw new cli_1.WorkspaceRequiredError(project.cwd, this.context.cwd);
       const command = this.cli.process([this.commandName, ...this.args]);
       const scriptName = command.path.length === 1 && command.path[0] === `run` && typeof command.scriptName !== `undefined` ? command.scriptName : null;
       if (command.path.length === 0) throw new clipanion_1.UsageError(`Invalid subcommand name for iteration - use the 'run' keyword if you wish to execute a script`);
@@ -292,15 +290,11 @@ module.exports = {
             let isRunnable = true;
 
             if (this.topological || this.topologicalDev) {
-              const resolvedSet = this.topologicalDev ? [...workspace.manifest.dependencies, ...workspace.manifest.devDependencies] : workspace.manifest.dependencies;
+              const resolvedSet = this.topologicalDev ? new Map([...workspace.manifest.dependencies, ...workspace.manifest.devDependencies]) : workspace.manifest.dependencies;
 
-              for (const [,
-              /*identHash*/
-              descriptor] of resolvedSet) {
-                const workspaces = project.findWorkspacesByDescriptor(descriptor);
-                isRunnable = !workspaces.some(workspace => {
-                  return needsProcessing.has(workspace.anchoredLocator.locatorHash);
-                });
+              for (const descriptor of resolvedSet.values()) {
+                const workspace = project.tryWorkspaceByDescriptor(descriptor);
+                isRunnable = workspace === null || !needsProcessing.has(workspace.anchoredLocator.locatorHash);
 
                 if (!isRunnable) {
                   break;
@@ -547,87 +541,6 @@ module.exports = {
   /***/ (function(module, exports) {
 
   module.exports = require("yup");
-
-  /***/ }),
-  /* 9 */
-  /***/ (function(module, exports, __webpack_require__) {
-
-  "use strict";
-
-
-  var __decorate = this && this.__decorate || function (decorators, target, key, desc) {
-    var c = arguments.length,
-        r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc,
-        d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-  };
-
-  Object.defineProperty(exports, "__esModule", {
-    value: true
-  });
-
-  const cli_1 = __webpack_require__(2);
-
-  const core_1 = __webpack_require__(3);
-
-  const core_2 = __webpack_require__(3);
-
-  const clipanion_1 = __webpack_require__(4); // eslint-disable-next-line arca/no-default-export
-
-
-  class WorkspaceCommand extends clipanion_1.Command {
-    constructor() {
-      super(...arguments);
-      this.args = [];
-    }
-
-    async execute() {
-      const configuration = await core_1.Configuration.find(this.context.cwd, this.context.plugins);
-      const {
-        project,
-        workspace: cwdWorkspace
-      } = await core_1.Project.find(configuration, this.context.cwd);
-      if (!cwdWorkspace) throw new cli_1.WorkspaceRequiredError(this.context.cwd);
-      const candidates = project.workspaces;
-      const candidatesByName = new Map(candidates.map(workspace => {
-        const ident = core_2.structUtils.convertToIdent(workspace.locator);
-        return [core_2.structUtils.stringifyIdent(ident), workspace];
-      }));
-      const workspace = candidatesByName.get(this.workspaceName);
-
-      if (workspace === undefined) {
-        const otherNames = Array.from(candidatesByName.keys()).sort();
-        throw new clipanion_1.UsageError(`Workspace '${this.workspaceName}' not found. Did you mean any of the following:\n  - ${otherNames.join("\n  - ")}?`);
-      }
-
-      return this.cli.run([this.commandName, ...this.args], {
-        cwd: workspace.cwd
-      });
-    }
-
-  }
-
-  WorkspaceCommand.usage = clipanion_1.Command.Usage({
-    category: `Workspace-related commands`,
-    description: `run a command within the specified workspace`,
-    details: `
-        > In order to use this command you need to add \`@yarnpkg/plugin-workspace-tools\` to your plugins.
-
-        This command will run a given sub-command on a single workspace.
-      `,
-    examples: [[`Add a package to a single workspace`, `yarn workspace components add -D react`], [`Run build script on a single workspace`, `yarn workspace components run build`]]
-  });
-
-  __decorate([clipanion_1.Command.String()], WorkspaceCommand.prototype, "workspaceName", void 0);
-
-  __decorate([clipanion_1.Command.String()], WorkspaceCommand.prototype, "commandName", void 0);
-
-  __decorate([clipanion_1.Command.Proxy()], WorkspaceCommand.prototype, "args", void 0);
-
-  __decorate([clipanion_1.Command.Path(`workspace`)], WorkspaceCommand.prototype, "execute", null);
-
-  exports.default = WorkspaceCommand;
 
   /***/ })
   /******/ ]);
